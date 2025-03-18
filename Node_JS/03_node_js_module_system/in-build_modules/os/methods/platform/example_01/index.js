ebb7b5b13f9943b173528aa49bdb497e44de871a
NSaha27 <sahaniladri27y@gmail.com>
2025-03-18 20:01:47 +0530

    examples on some node built-in modules

---
+++ Node_JS/03_node_js_module_system/in-build_modules/os/methods/platform/example_01/index.js
@@ -0,0 +1,10 @@
+/*
+  os.platform()
+  --------------
+
+  This method returns the current platform of our system's operating system.
+*/
+
+import os from "os";
+
+console.log(`The current platform of my os : ${os.platform()}`);
